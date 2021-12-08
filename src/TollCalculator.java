916f993e53c6943a1e05f81012458678de4d02e8
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2021-12-08 17:53:52 +0100

    Update TollCalculator.java

--- src/TollCalculator.java
+++ src/TollCalculator.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class TollCalculator {
 
@@ -10,22 +11,14 @@ public class TollCalculator {
         for (LocalDateTime date : dates) {
             currentFee = countFeeVehicleType(vehicle, date);
             totalFee += currentFee;
-            System.out.println(currentFee);
+            int[] myArray = new int[] { currentFee };
+            System.out.println(Arrays.toString(myArray));
         }
         if (totalFee > 60)
             totalFee = 60;
         System.out.println("totalFee: " + totalFee);
         return totalFee;
     }
-
-    // public int TollCalcTest(Vehicle vehicle, LocalDateTime[] dates) {
-
-    //     for (LocalDateTime date : dates) {
-
-    //     }
-
-    //     return 0;
-    // }
 
     private int countFeeVehicleType(Vehicle vehicle, LocalDateTime date) {
         if (date.getHour() == 8 || date.getHour() == 16) {
