407b73dae5d05c39893d93c506a9d19bea470876
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2021-12-08 18:11:47 +0100

    Update TollCalculatorTest.java
    Added:
    +Max fee test.

--- src/TollCalculatorTest.java
+++ src/TollCalculatorTest.java
@@ -11,7 +11,7 @@ public class TollCalculatorTest {
     public void testhour() {
         TollCalculator tc = new TollCalculator();
         Vehicle car = new Car();
-        
+
         LocalDateTime[] hour8 = {
                 LocalDateTime.of(2021, 12, 8, 8, 10, 0),
         };
@@ -20,14 +20,23 @@ public class TollCalculatorTest {
                 LocalDateTime.of(2021, 12, 8, 16, 10, 0),
         };
 
+        LocalDateTime[] max60= {
+            LocalDateTime.of(2021, 12, 8, 16, 10, 0),
+            LocalDateTime.of(2021, 12, 8, 16, 10, 0),
+            LocalDateTime.of(2021, 12, 8, 16, 10, 0),
+            LocalDateTime.of(2021, 12, 8, 16, 10, 0),
+            LocalDateTime.of(2021, 12, 8, 16, 10, 0),
+    };
+
         int t8 = tc.getTollFee(car, hour8);
         int t16 = tc.getTollFee(car, hour16);
+        int tmax60fee = tc.getTollFee(car, max60);
 
         // Actual
-        int[] actual = { t8, t16 };
+        int[] actual = { t8, t16, tmax60fee};
 
         // Expected
-        int[] expected = { 18, 18 };
+        int[] expected = { 18, 18, 60};
 
         // Assert
         Assert.assertArrayEquals(actual, expected);
